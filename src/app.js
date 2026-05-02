5f4cd89244e362333229a8d0f52e65770a3e6bc2
Your New Name <NOMAN@GMAIL.com>
2026-05-02 18:23:15 +0500

    add core

--- src/app.js
+++ src/app.js
@@ -13,6 +13,13 @@ const adminRoutes = require('./routes/adminRoutes'); // New
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // یا اپنا ورسل کا URL ڈالیں
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
